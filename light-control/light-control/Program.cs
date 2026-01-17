e1576fd94a9a1391f174fedc6b68755c9b5e18e0
sirmpixx <dev@sirmpixx.de>
2026-01-17 14:56:33 +0100

    Removed ChatGPT Code

--- light-control/light-control/Program.cs
+++ light-control/light-control/Program.cs
@@ -1,8 +1,5 @@
 ﻿using DotNetEnv;
 using System;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace light_control
@@ -19,7 +16,6 @@ namespace light_control
         {
             Env.Load(); // lädt .env automatisch
 
-            SetHueStateAsync(true).GetAwaiter().GetResult();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             // Application.Run(new mainWindow());
@@ -31,19 +27,5 @@ namespace light_control
             string strHueAPI = Environment.GetEnvironmentVariable("HUE_API_KEY");
             return "http://" + strHueIP + "/api/" + strHueAPI;
         }
-
-        public static async Task SetHueStateAsync(bool on)
-        {
-            string baseUrl = GetHueApiUrl();
-            string url = baseUrl + "/lights/6/state";
-            string json = "{ \"on\": " + on.ToString().ToLower() + " }";
-
-            using (HttpClient client = new HttpClient())
-            using (StringContent content = new StringContent(json, Encoding.UTF8, "application/json"))
-            {
-                await client.PutAsync(url, content);
-            }
-        }
-
     }
 }
